b39d3f5a0f69251a1b7c3a496f8b78ebb20e83cc
odekeyser <odekeyser@localhost>
2014-02-22 23:05:40 +0100

    ajout d'un actionListenner au bouton.

--- src/be/ac/ucl/lfsab1509/memogeo/MainActivity.java
+++ src/be/ac/ucl/lfsab1509/memogeo/MainActivity.java
@@ -3,18 +3,34 @@ package be.ac.ucl.lfsab1509.memogeo;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity 
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+                button.setText("test");//performe une action quand on clique sur le bouton. Ici, changer le texte du bouton.
+            }            	
+        });
     }
+    
+    
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu) 
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
