409807a265b3356776025184a429693d690176c0
odekeyser <odekeyser@localhost>
2014-03-06 10:52:00 +0100

    popup pour choisir l'heure.

--- src/be/ac/ucl/lfsab1509/memogeo/OptionsActivity.java
+++ src/be/ac/ucl/lfsab1509/memogeo/OptionsActivity.java
@@ -3,16 +3,22 @@ package be.ac.ucl.lfsab1509.memogeo;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v4.app.DialogFragment;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
-public class OptionsActivity extends Activity {
+public class OptionsActivity extends Activity implements View.OnClickListener {
 
+	private Button chooseTime;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_options);		
+		setContentView(R.layout.activity_options);
+		
+		this.chooseTime = (Button) findViewById(R.id.buttonTime);
+		this.chooseTime.setOnClickListener(this);
 	}
 	
 	@Override
@@ -20,6 +26,19 @@ public class OptionsActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.options, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		
+		switch (v.getId())
+		{
+			case R.id.buttonTime:
+				ChooseTime chooseTime = new ChooseTime();
+				chooseTime.show(getFragmentManager(), "Time");
+				break;
+		}
+		
 	}
 
 }
