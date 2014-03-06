479ab8ecf48bc300960114d1be462afa45fc45d2
odekeyser <odekeyser@localhost>
2014-03-06 09:28:45 +0100

    Bouton pour arriver au menu de date et heure fonctionnel.

--- src/be/ac/ucl/lfsab1509/memogeo/OptionsActivity.java
+++ src/be/ac/ucl/lfsab1509/memogeo/OptionsActivity.java
@@ -2,7 +2,10 @@ package be.ac.ucl.lfsab1509.memogeo;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class OptionsActivity extends Activity {
 
@@ -10,8 +13,20 @@ public class OptionsActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_options);
+		
+		// Bouton pour arriver dans le choix de la date et de l'heure.
+		
+		final Button When = (Button) findViewById(R.id.buttonChooseTime);
+		When.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {               
+            	Intent WriteNewMemo = new Intent(OptionsActivity.this, TimeOptionActivity.class);
+             	startActivity(WriteNewMemo);
+            }            	
+        });
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
