33c50697ef986406638197fe876f4fc486b75e26
odekeyser <odekeyser@localhost>
2014-03-08 12:00:45 +0100

    re push des fichiers

---
+++ src/be/ac/ucl/lfsab1509/memogeo/ChooseTime.java
@@ -0,0 +1,49 @@
+package be.ac.ucl.lfsab1509.memogeo;
+
+import java.util.Calendar;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.widget.TimePicker;
+
+public class ChooseTime extends DialogFragment implements TimePickerDialog.OnTimeSetListener
+{
+	@Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+	
+	@Override
+	public void onTimeSet(TimePicker view, int hourOfDay, int minute) 
+	{
+
+	    String min = minute+"";
+		
+	    if (minute < 10)
+		{
+			min = "0"+minute;
+		}
+		
+		String hour = hourOfDay+"";
+		
+		if (hourOfDay < 10)
+		{
+			hour = "0"+hourOfDay;
+		}
+		
+		// Generate the string Time for the optionsActivity class
+		((OptionsActivity) getActivity()).EditTextTime(hour+":"+min);
+		
+	}
+
+}
---
+++ src/be/ac/ucl/lfsab1509/memogeo/Memo.java
@@ -0,0 +1,127 @@
+package be.ac.ucl.lfsab1509.memogeo;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+
+public class Memo implements Parcelable
+{
+	private String title;
+	private String memo;
+	private String address;
+	private double longitude;
+	private double latitude;
+	private String time;
+	private String date;
+	
+	// Constructor
+	public Memo(String title, String memo, String address, double longitude, double latitude, String time, String date)
+	{
+		this.title = title;
+		this.memo = memo;
+		this.address = address;
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.time = time;
+		this.date = date;
+	}
+	
+	//Default constructor
+	public Memo(){}
+	
+	// Getters and setters
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getMemo() {
+		return memo;
+	}
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	// To String method.
+	@Override
+	public String toString() 
+	{
+		return "Memo [title" + title + "memo=" + memo + ", address=" + address + ", longitude="
+				+ longitude + ", latitude=" + latitude + ", time=" + time + ", date=" + date + "]";
+	}
+
+	@Override
+	public int describeContents() 
+	{
+		// Return 0 because the class does not contain a FileDescriptor.
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel destination, int flags)
+	{
+		destination.writeString(title);
+		destination.writeString(memo);
+		destination.writeString(address);
+		destination.writeDouble(longitude);
+		destination.writeDouble(latitude);
+		destination.writeString(time);
+		destination.writeString(date);
+	}	
+	
+	public static final Parcelable.Creator<Memo> CREATOR = new Parcelable.Creator<Memo>()
+	{
+		@Override
+		public Memo createFromParcel(Parcel source) {
+		return new Memo(source);
+		}
+		
+		@Override
+		public Memo[] newArray(int size) {
+		return new Memo[size];
+		}
+	};
+	
+	public Memo(Parcel in)
+	{
+		title = in.readString();
+		memo = in.readString();
+		address = in.readString();
+		longitude = in.readDouble();
+		latitude = in.readDouble();
+		time = in.readString();
+		date = in.readString();
+	}
+}
