4124dac01354042f5cfdd5c175ae0c426ae51ddc
adrien theatre <adrien.theatre@student.uclouvain.be>
2014-03-25 16:14:11 +0100

    map updated

--- src/be/ac/ucl/lfsab1509/memogeo/Map.java
+++ src/be/ac/ucl/lfsab1509/memogeo/Map.java
@@ -2,40 +2,45 @@ package be.ac.ucl.lfsab1509.memogeo;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Fragment;
 import android.view.Menu;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.Overlay;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 public class Map extends Activity {
 
-    @Override
+	/**GoogleMap mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+	//localisation reaumur hardcode
+	private static final LatLng REAUMUR = new LatLng(50.668498, 4.622155);
+	float zoom = 15;*/
+    
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+        setContentView(R.layout.activity_map);     
+        
     }
+    
+	
+	
+	/**copié du site android sert à vérifier si on a une map avant de la manipuler
+    private void setUpMapIfNeeded() {
+        // Vérifier si la map existe
+        if (mMap == null) {
+            mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                                .getMap();
+            // Vérifier si on a pu récupérer la map
+            if (mMap != null) {
+            	mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(REAUMUR, zoom));
+            	mMap.setMyLocationEnabled(true);
 
-/** public class Map extends MapActivity {
+            }
+        }
+    }*/
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_map);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	  protected boolean isRouteDisplayed() {
-	    return false;
-	  }
-
-	  @Override
-	  protected boolean isLocationDisplayed() {
-	    return true;
-	  }
-	  */
 }
