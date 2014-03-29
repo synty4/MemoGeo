da6872f096e522810f92a2476d977d9ddd6986c2
Synty4 <Synty4@localhost>
2014-03-29 20:11:11 +0100

    Merge  Synty+ Merlin + Olivier (Nouvelle activit? pour ma partie.)

--- src/be/ac/ucl/lfsab1509/memogeo/MainActivity.java
+++ src/be/ac/ucl/lfsab1509/memogeo/MainActivity.java
@@ -13,50 +13,74 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity implements View.OnClickListener{
+
+ 	private Button newMemo;
+ 	private Button map;
+ 	private Button memoList;
+    private Button timers;
+
 
 		@Override
 		public void onCreate(Bundle savedInstanceState)
 		{
-			super.onCreate(savedInstanceState); 
+		    super.onCreate(savedInstanceState); 
 			//requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
 			setContentView(R.layout.activity_main);  
 			//getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,    R.layout.title_layout);
-			
 		
-		
-		/*test affichage dans l'onglet log*/
-		 DatabaseHandler db;
-		 db = new DatabaseHandler(this);
+		    /*test affichage dans l'onglet log*/
+		    DatabaseHandler db;
+		    db = new DatabaseHandler(this);
 		 
+
+	 		this.newMemo = (Button) findViewById(R.id.new_memo);
+	 		this.newMemo.setOnClickListener(this);
+	 		
+	 		this.map = (Button) findViewById(R.id.open_map);
+	 		this.map.setOnClickListener(this);
+	 		
+	 		this.memoList = (Button) findViewById(R.id.list_memo);
+	 		this.memoList.setOnClickListener(this);
+	 		
+	 		this.timers = (Button) findViewById(R.id.timers);
+            this.timers.setOnClickListener(this);
+            
+         
+
+
 		 
-		Log.d("Insert: ", "Inserting .."); 
-		//MemoInformation memoInfoUn = new MemoInformation("Title: acheter medicament","description: pour mal de tete ","Traverse d'Esope 5, 1348");
-		MemoInformation memoInfoUn = new MemoInformation(" acheter medicament","pour mal de tete ","Traverse d'Esope 5, 1348");
-		db.addMemoInformation(memoInfoUn); 
-	    Log.d("ONE ************************** ", "**********************"); 
+		    Log.d("Insert: ", "Inserting .."); 
+		    //MemoInformation memoInfoUn = new MemoInformation("Title: acheter medicament","description: pour mal de tete ","Traverse d'Esope 5, 1348");
+		    MemoInformation memoInfoUn = new MemoInformation(" acheter medicament","pour mal de tete ","Traverse d'Esope 5, 1348");
+		    
+		    db.addMemoInformation(memoInfoUn); 
+	        Log.d("ONE ************************** ", "**********************"); 
 		
-	    MemoInformation memoInfoDeux = new MemoInformation("livre calculabilité","biblio INGI "," Place Sainte Barbe 2, 1348");
-	    db.addMemoInformation(memoInfoDeux); 
-	    Log.d("TWO ************************** ", "**********************"); 
+	        MemoInformation memoInfoDeux = new MemoInformation("livre calculabilité","biblio INGI "," Place Sainte Barbe 2, 1348");
+	        db.addMemoInformation(memoInfoDeux); 
+	        Log.d("TWO ************************** ", "**********************"); 
     
-	    /*test suppression MemoInformation dans l'onglet log:  suppression et affichage après suppression*/
-	   /* Log.d("************ DELETE ************** ", "**********************"); 
-	    Log.d("idd",String.valueOf(memoInfoUn.getId()));
-	    Log.d("titles",memoInfoUn.getTitle());*/
+	       /*test suppression MemoInformation dans l'onglet log:  suppression et affichage après suppression*/
+	        /* Log.d("************ DELETE ************** ", "**********************"); 
+	        Log.d("idd",String.valueOf(memoInfoUn.getId()));
+	        Log.d("titles",memoInfoUn.getTitle());*/
 	    
-	    Log.d("Reading: ", "Reading all memoInfo.."); 
-        List<MemoInformation> memoInfo = db.getAllMemoInformation(); 
-         	for (MemoInformation i : memoInfo) {
+	         Log.d("Reading: ", "Reading all memoInfo.."); 
+             List<MemoInformation> memoInfo = db.getAllMemoInformation(); 
+             
+         	 for (MemoInformation i : memoInfo) {
          		String log = i.toString();
 	            
          		// Writing MemoInformation to log
 	            Log.d("Title: ", log);
          	}
-	    
+		}
 	   
-	   
+	   /**
 		final Button NewMemo = (Button) findViewById(R.id.new_memo);// A changer par méthode plus propre
 		NewMemo.setOnClickListener(new View.OnClickListener() 
         {
@@ -77,25 +101,60 @@ public class MainActivity extends Activity {
              	startActivity(Map);
             }            	
         });
-		
+		**/
 		// button to list memo  in the main activity
-					final Button ListMemo = (Button) findViewById(R.id.list_memo);// A changer par méthode plus propre
-					ListMemo.setOnClickListener(new View.OnClickListener() 
-			        {
+					/**final Button ListMemo = (Button) findViewById(R.id.list_memo);// A changer par méthode plus propre
+					ListMemo.setOnClickListener(new View.OnClickListener()**/
+	 
+         	
+	     	@Override
+         	public void onClick(View v) {
+				 		switch (v.getId()) {
+
+				 		case R.id.new_memo:
+				 			Intent WriteNewMemo = new Intent(MainActivity.this,WriteNewMemo.class);
+				 			startActivity(WriteNewMemo);
+				 			break;
+				 		case R.id.open_map:
+				 			Intent Map = new Intent(MainActivity.this, Map.class);
+				 			startActivity(Map);
+				 			break;
+				 		case R.id.list_memo:
+				 			Intent ListMemo = new Intent(MainActivity.this, ListMemo.class);
+			            	startActivity(ListMemo);
+                            break;
+                		case R.id.timers:
+                        	Intent Timers = new Intent(MainActivity.this, 
+                        	TimersActivity.class );
+				            startActivity(Timers);
+				            break;
+
+				 		}
+
+				 	}
+
+
+
+			       /** {
 			            public void onClick(View v) 
 			            {               
 			            	Intent ListMemo = new Intent(MainActivity.this, ListMemo.class);
 			            	startActivity(ListMemo);
 			            }            	
-			        });
+			        });**/
 		
 		     
-	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		return true;
 	}
+
+	
+	
+
+	
 
 }
