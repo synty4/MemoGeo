6ff67c5c2899d9604bf5a715e335d8ef323443bc
odekeyser <odekeyser@localhost>
2014-04-13 19:58:46 +0200

    j'arrive ? retirer notre position GPS.

--- src/be/ac/ucl/lfsab1509/memogeo/TimersActivity.java
+++ src/be/ac/ucl/lfsab1509/memogeo/TimersActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.location.LocationManager;
 import android.media.MediaPlayer;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,7 +13,12 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
 import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationClient.OnAddGeofencesResultListener;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -21,7 +27,8 @@ import android.content.Context;
 import android.content.IntentFilter;
 import android.widget.Toast;
 
-public class TimersActivity extends Activity implements View.OnClickListener {
+public class TimersActivity extends Activity implements View.OnClickListener{
+
 	private Button chooseTime;
 	private Button chooseDate;
 	private Button openMap;
@@ -32,29 +39,16 @@ public class TimersActivity extends Activity implements View.OnClickListener {
 	private MediaPlayer mp = new MediaPlayer();
 
 	AlarmManager manager;
-	PendingIntent pendingIntent;
+	PendingIntent timePendingIntent, geoPendingIntent;
 	Context mContext;
 	BroadcastReceiver br;
-	
-	/*
-     * Internal geofence objects for geofence 1 and 2
-     */
-    private SimpleGeofence mUIGeofence1;
-    private SimpleGeofence mUIGeofence2;
-    
-    // Internal List of Geofence objects
-    List<Geofence> mGeofenceList;
-    // Persistent storage for geofences
-    private SimpleGeofenceStore mGeofenceStorage;
-	
-	/*
-	 *Test values for the geofence.
-	 */
-	
-	double latTest = 50.770019;
-	double longTest = 4.650283;
-	float radius = 100;
- 
+
+	double latTest = 50.768229;// Arbre de la maison
+	double longTest = 4.647869;
+	double latTestHome = 50.768219;// Ma maison
+	double longTestHome = 4.648526;
+	float radius = 10;
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_timers);
@@ -62,12 +56,6 @@ public class TimersActivity extends Activity implements View.OnClickListener {
 		mp = MediaPlayer.create(this, R.raw.gameover);// Crée un MediaPlayer qui
 														// lira le fichier audio
 														// du dossier raw
-		
-		// Instantiate a new geofence storage area
-        mGeofenceStorage = new SimpleGeofenceStore(this);
-
-        // Instantiate the current List of geofences
-        ArrayList<Geofence> mCurrentGeofences = new ArrayList<Geofence>();
 
 		this.chooseTime = (Button) findViewById(R.id.buttonTime);// Bouton et
 																	// champ de
@@ -96,11 +84,10 @@ public class TimersActivity extends Activity implements View.OnClickListener {
 		this.geoTimer = (Button) findViewById(R.id.buttonGeoTimer);// Bouton Geo
 																	// Timer
 		this.geoTimer.setOnClickListener(this);
-		
-		
+
 	}
 
-	//Prépare the alarm.
+	// Prépare the alarm.
 	private void setup(String memo) {
 		final String title = memo;
 
@@ -114,12 +101,13 @@ public class TimersActivity extends Activity implements View.OnClickListener {
 			}
 		};
 		registerReceiver(br, new IntentFilter("be.ac.ucl.lfsab1509.memogeo"));
-		pendingIntent = PendingIntent.getBroadcast(this, 0, new Intent(
-				"be.ac.ucl.lfsab1509.memogeo"), 0);
-		manager = (AlarmManager) (this.getSystemService(Context.ALARM_SERVICE));
+		// timePendingIntent = PendingIntent.getBroadcast(this, 0, new
+		// Intent("be.ac.ucl.lfsab1509.memogeo"), 0);
+		// manager = (AlarmManager)
+		// (this.getSystemService(Context.ALARM_SERVICE));
 	}
 
-	//OnClick listeners
+	// OnClick listeners
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.buttonTime:
@@ -140,8 +128,9 @@ public class TimersActivity extends Activity implements View.OnClickListener {
 
 		case R.id.buttonTimeTimer:
 
-			Toast.makeText(getApplicationContext(), "Launch timer",
+			Toast.makeText(getApplicationContext(), "Launch time timer",
 					Toast.LENGTH_LONG).show();
+
 			Memo timedMemo = new Memo();// Cr�� un objet m�mo
 
 			timedMemo.setTime("" + selectTime.getText().toString());// Ajoute le
@@ -176,14 +165,51 @@ public class TimersActivity extends Activity implements View.OnClickListener {
 															// month, int day,
 															// int hourOfDay,
 															// int minute, int
-			// second)
+															// second)
 			setup(timedMemo.getMemo());
+			timePendingIntent = PendingIntent.getBroadcast(this, 0, new Intent(
+					"be.ac.ucl.lfsab1509.memogeo"), 0);
+			manager = (AlarmManager) (this
+					.getSystemService(Context.ALARM_SERVICE));
 			manager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
-					pendingIntent);
+					timePendingIntent);
 
 			break;
 
 		case R.id.buttonGeoTimer:
+
+			/**
+			Toast.makeText(getApplicationContext(), "Launch géo timer",
+					Toast.LENGTH_LONG).show();
+
+			Memo geoMemo = new Memo();// Cr�� un objet m�mo
+			geoMemo.setMemo("Test Geo Memo");// Permet de tester le m�mo
+
+			setup(geoMemo.getMemo());
+
+			geoPendingIntent = PendingIntent.getBroadcast(this, 0, new Intent(
+					"be.ac.ucl.lfsab1509.memogeo"), 0);
+			// On ajoute une alerte de proximité si on est à 15m du point.
+			LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+			lm.addProximityAlert(latTest, longTest, radius, -1,
+					geoPendingIntent);
+					
+			**/
+			GPSTracker gps = new GPSTracker(this);
+			
+			if(gps.canGetLocation()){
+                
+                double latitude = gps.getLatitude();
+                double longitude = gps.getLongitude();
+                 
+                // \n is for new line
+                Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();    
+            }else{
+                // can't get location
+                // GPS or Network is not enabled
+                // Ask user to enable GPS/network in settings
+                gps.showSettingsAlert();
+            }
 
 			break;
 		}
@@ -199,27 +225,4 @@ public class TimersActivity extends Activity implements View.OnClickListener {
 	public void EditTextDate(String date) {
 		this.selectDate.setText(date);
 	}
-
-	/**
-	 * Get the geofence parameters for each geofence from the UI and add them to
-	 * a List.
-	 */
-	public void createGeofences() {
-		/*
-		 * Create an internal object to store the data. Set its ID to "1". This
-		 * is a "flattened" object that contains a set of strings
-		 */
-		mUIGeofence1 = new SimpleGeofence("1",latTest, longTest, 10, 600000, Geofence.GEOFENCE_TRANSITION_ENTER);
-				// This geofence records only entry transitions
-		// Store this flat version
-		mGeofenceStorage.setGeofence("1", mUIGeofence1);
-		// Create another internal object. Set its ID to "2"
-		mUIGeofence2 = new SimpleGeofence("2",latTest, longTest, 10, 600000, Geofence.GEOFENCE_TRANSITION_EXIT);
-				// This geofence records exit transitions
-		// Store this flat version
-		mGeofenceStorage.setGeofence("2", mUIGeofence2);
-		mGeofenceList.add(mUIGeofence1.toGeofence());
-		mGeofenceList.add(mUIGeofence2.toGeofence());
-	}
-
 }
